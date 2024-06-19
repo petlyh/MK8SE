732d69756d0241e4cf85e93d88efc4f303d43e6c
petlyh <88139840+petlyh@users.noreply.github.com>
2024-06-19 21:28:37 +0200

    Add app icon for Linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -7,6 +7,11 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
+#include <filesystem>
+
+using namespace std;
+using namespace std::filesystem;
+
 struct _MyApplication {
   GtkApplication parent_instance;
   char** dart_entrypoint_arguments;
@@ -19,6 +24,12 @@ static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+
+  // custom icon
+  const string iconFilename = "assets/icon.png";
+  path execDir = canonical(read_symlink("/proc/self/exe")).parent_path();
+  path iconPath = execDir / "data/flutter_assets" / iconFilename;
+  gtk_window_set_icon_from_file(GTK_WINDOW(window), iconPath.c_str(), NULL);
 
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
